a738ffb909c1600a6876ed1fca6c6a537ea82e71
Akshata Kodak <akshatakodak@gmail.com>
2024-02-05 17:51:24 +0530

    component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConnectComponent } from './connect/connect.component';
+import { HeaderComponent } from './header/header.component';
+import { QueryComponent } from './query/query.component';
+import { QueryparameterComponent } from './queryparameter/queryparameter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnectComponent,
+    HeaderComponent,
+    QueryComponent,
+    QueryparameterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/queryparameter/queryparameter.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { QueryparameterComponent } from './queryparameter.component';
+
+describe('QueryparameterComponent', () => {
+  let component: QueryparameterComponent;
+  let fixture: ComponentFixture<QueryparameterComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [QueryparameterComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(QueryparameterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
